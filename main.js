1a7f3c9f01440a9c14dd4510bae697a5ffe9854c
Brett Jory <bjory7@gmail.com>
2019-03-25 19:36:21 -0700

    assignment completed

---
+++ main.js
@@ -0,0 +1,29 @@
+function random_color(){
+    var rgb = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+    color = '#'  //this is what we'll return!
+    for(var i = 0; i < 6; i++) {
+        x = Math.floor((Math.random()*16))
+        color += rgb[x]; 
+    }
+    return color;
+};
+
+$(document).ready(function(){
+    $('#large_box').click(function(){
+        var color = random_color();
+        $(this).css('backgroundColor',color);
+        color = random_color();
+        $(this).children().css('backgroundColor',color);  
+    //comment this out when you have figured out what event.stopPropagation is used for
+    })
+    $('.side_box').click(function(event){
+        event.stopPropagation();
+        var color = random_color();
+        $(this).siblings().css('backgroundColor',color);
+    })
+    $('.middle_box').click(function(event){
+        event.stopPropagation();
+        var color = random_color();
+        $(this).parent().css('backgroundColor',color);
+    })
+});
